5b253bda014441a6a165e242b0957ea4bda63df3
Simeao Almeida <simer27@yahoo.com>
2024-10-11 19:39:27 -0300

    integração com o Prisma/SQLite e criada a primeira rota de criação de membros do time

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { PrismaService } from './database/prisma.service';
 
-@Controller()
+@Controller('/app')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private prisma: PrismaService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/member')
+  async getMember() {
+    const member = await this.prisma.membroDoTime.create({
+      data: {
+        id: 'lateral',
+        name: 'junior',
+        function: 'marcação, se lançar a frente e cruzar',
+      },
+    });
+
+    return { member };
   }
 }
